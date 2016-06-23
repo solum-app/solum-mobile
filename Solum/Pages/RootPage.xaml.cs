1e05942c5b6dec33ef3627646e4475ca100b8832
Igor Trindade <igor@sydy.com.br>
2016-06-23 17:23:42 -0400

    Ajustes no menu lateral, adicionado página sobre.

--- Solum/Pages/RootPage.xaml.cs
+++ Solum/Pages/RootPage.xaml.cs
@@ -1,20 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using Solum.Pages;
 using Xamarin.Forms;
 
 namespace Solum
 {
 	public partial class RootPage : MasterDetailPage
 	{
+		NavigationPage navigationPage;
+		Page currentPage;
+
 		public RootPage ()
 		{
 			InitializeComponent ();
 
-			Detail = new NavigationPage(new AnalisesPage ()){
+			currentPage = new AnalisesPage();
+
+			navigationPage = new NavigationPage(currentPage){
 				BarBackgroundColor = Color.FromHex("#24BE55"),
 				BarTextColor = Color.White
 			};
+
+			Detail = navigationPage;
+		}
+
+		async void OnAnalisesTapped(object sender, EventArgs e){
+			if (Device.OS == TargetPlatform.iOS) {
+				if (currentPage.GetType() == typeof(AnalisesPage))
+				{
+					IsPresented = false;
+				}
+				else {
+					currentPage = new AnalisesPage();
+					Detail = new NavigationPage(currentPage)
+					{
+						BarBackgroundColor = Color.FromHex("#24BE55"),
+						BarTextColor = Color.White
+					};
+					IsPresented = false;
+				}
+			} else {
+				var page = new AnalisesPage();
+				await navigationPage.Navigation.PushAsync(page);
+				navigationPage.Navigation.RemovePage(currentPage);
+				currentPage = page;
+
+				IsPresented = false;
+			}
+		}
+
+		//void OnNovaAnaliseTapped(object sender, EventArgs e)
+		//{
+		//	if (currentPage.GetType() == typeof(AnalisePage))
+		//	{
+		//		IsPresented = false;
+		//	}
+		//	else {
+		//		currentPage = new AnalisePage();
+		//		Detail = new NavigationPage(currentPage)
+		//		{
+		//			BarBackgroundColor = Color.FromHex("#24BE55"),
+		//			BarTextColor = Color.White
+		//		};
+		//		IsPresented = false;
+		//	}
+		//}
+
+		async void OnSobreTapped(object sender, EventArgs e)
+		{
+			if (Device.OS == TargetPlatform.iOS)
+			{
+				if (currentPage.GetType() == typeof(SobrePage))
+				{
+					IsPresented = false;
+				}
+				else {
+					currentPage = new SobrePage();
+					Detail = new NavigationPage(currentPage)
+					{
+						BarBackgroundColor = Color.FromHex("#24BE55"),
+						BarTextColor = Color.White
+					};
+					IsPresented = false;
+				}
+			}
+			else {
+				var page = new SobrePage();
+				await navigationPage.Navigation.PushAsync(page);
+				navigationPage.Navigation.RemovePage(currentPage);
+				currentPage = page;
+
+				IsPresented = false;
+			}
 		}
 	}
 }
---
+++ Solum/Pages/SobrePage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Solum
+{
+	public partial class SobrePage : ContentPage
+	{
+		public SobrePage()
+		{
+			InitializeComponent();
+
+			Task.Run(() => LoadHtml ());
+		}
+
+		void LoadHtml(){
+			var assembly = typeof(SobrePage).GetTypeInfo().Assembly;
+			Stream stream = assembly.GetManifestResourceStream("Solum.Resources.sobre.html");
+			var html = "";
+			using (var reader = new StreamReader(stream))
+			{
+				html = reader.ReadToEnd();
+			}
+
+			webView.Source = new HtmlWebViewSource()
+			{
+				Html = html
+			};
+		}
+
+	}
+}
+
