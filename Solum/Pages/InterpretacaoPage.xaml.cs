c04920e9e14a194d71d88b799b43881bbbff2b72
sidneipolo <sidneipolo.sp@gmail.com>
2016-06-23 17:26:33 -0400

    Relatório implementado

--- Solum/Pages/InterpretacaoPage.xaml.cs
+++ Solum/Pages/InterpretacaoPage.xaml.cs
@@ -10,6 +10,7 @@ using System.IO;
 using Solum.Interfaces;
 using System.Reflection;
 using System;
+using Solum.Handlers;
 
 namespace Solum.Pages
 {
@@ -17,20 +18,24 @@ namespace Solum.Pages
 	{
 		PdfDocument doc;
 		PdfPage page;
-		Syncfusion.Drawing.Color headerColor = Syncfusion.Drawing.Color.FromArgb(255, 71, 201, 173);
-
-		Syncfusion.Drawing.Color black = Syncfusion.Drawing.Color.FromArgb(255, 0, 0, 0);
+		Syncfusion.Drawing.Color green = Syncfusion.Drawing.Color.FromArgb (255, 63, 170, 88);
+		Syncfusion.Drawing.Color black = Syncfusion.Drawing.Color.FromArgb(255, 52, 52, 52);
 		Syncfusion.Drawing.Color white = Syncfusion.Drawing.Color.FromArgb(255, 255, 255, 255);
-		Syncfusion.Drawing.Color violet = Syncfusion.Drawing.Color.FromArgb(255, 151, 108, 174);
+		Syncfusion.Drawing.Color gray = Syncfusion.Drawing.Color.FromArgb(255, 155, 155, 155);
+		Syncfusion.Drawing.Color grayLight = Syncfusion.Drawing.Color.FromArgb (255, 241, 241, 241);
+
+		Analise analise;
 
 		public InterpretacaoPage (Analise analise, bool isVisualizacao)
 		{
+			this.analise = analise;
 			BindingContext = new InterpretacaoViewModel (Navigation, analise);
 			Init (isVisualizacao);
 		}
 
 		public InterpretacaoPage (Analise analise, Analise realmAnalise, bool isVisualizacao)
 		{
+			this.analise = analise;
 			BindingContext = new InterpretacaoViewModel (Navigation, analise, realmAnalise);
 			Init (isVisualizacao);
 		}
@@ -81,83 +86,113 @@ namespace Solum.Pages
 			page = doc.Pages.Add();
 			PdfGraphics g = page.Graphics;
 
-			PdfFont headerFont = new PdfStandardFont(PdfFontFamily.Helvetica, 35);
-			PdfFont subHeadingFont = new PdfStandardFont(PdfFontFamily.Helvetica, 16);
-			g.DrawRectangle(new PdfSolidBrush(headerColor), new RectangleF(0, 0, page.Graphics.ClientSize.Width, page.Graphics.ClientSize.Height));
-			g.DrawRectangle(new PdfSolidBrush(white), new RectangleF(0, 100, page.Graphics.ClientSize.Width, page.Graphics.ClientSize.Height - 150));
-			g.DrawString("Enterprise", headerFont, new PdfSolidBrush(violet), new PointF(10, 20));
-			g.DrawString("Reporting Solutions", subHeadingFont, new PdfSolidBrush(black), new PointF(15, 70));
+			var assembly = typeof (App).GetTypeInfo ().Assembly;
+			Stream imgStream = assembly.GetManifestResourceStream ("Solum.ic_solum.jpg");
+			g.DrawImage (PdfImage.FromStream (imgStream), 20, 20, 72, 72);
 
-			g.DrawString(String.Format("{0:dd/MM/yyyy}", DateTime.Today), subHeadingFont, new PdfSolidBrush(white), new PointF(page.Graphics.ClientSize.Width - 100, 20));
+			PdfFont headerFont = new PdfStandardFont(PdfFontFamily.Helvetica, 16, PdfFontStyle.Bold);
+			PdfFont subHeadingFont = new PdfStandardFont(PdfFontFamily.Helvetica, 14);
+			PdfFont textFont = new PdfStandardFont (PdfFontFamily.Helvetica, 12);
 
-//			PdfLayoutResult result = HeaderPoints("Develop cloud-ready reporting applications in as little as 20% of the time.", 15);
-//			result = HeaderPoints("Proven, reliable platform thousands of users over the past 10 years.", result.Bounds.Bottom + 15);
-//			result = HeaderPoints("Microsoft Excel, Word, Adobe PDF, RDL display and editing.", result.Bounds.Bottom + 15);
-//			result = HeaderPoints("Why start from scratch? Rely on our dependable solution frameworks", result.Bounds.Bottom + 15);
+			//Header 
+			g.DrawString ("Relatório de iterpretação de análise de solo", headerFont, new PdfSolidBrush (black), new PointF (110, 20));
+			g.DrawString (analise.Fazenda, subHeadingFont, new PdfSolidBrush (black), new PointF (110, 52));
+			g.DrawString ("Talhão: " + analise.Talhao, textFont, new PdfSolidBrush (black), new PointF (110, 74));
+			g.DrawString (String.Format ("{0:dd/MM/yyyy}", analise.Data), textFont, new PdfSolidBrush (black), new PointF (page.Graphics.ClientSize.Width - 80, 25));
+			g.DrawRectangle (new PdfSolidBrush (green), new RectangleF (0, 105, page.Graphics.ClientSize.Width, 5));
 
-			PdfLayoutResult result = BodyContent("Deployment-ready framework tailored to your needs.", 100);
-			result = BodyContent("Our architects and developers have years of reporting experience.", result.Bounds.Bottom + 25);
-			result = BodyContent("Solutions available for web, desktop, and mobile applications.", result.Bounds.Bottom + 25);
-			result = BodyContent("Backed by our end-to-end product maintenance infrastructure.", result.Bounds.Bottom + 25);
-			result = BodyContent("The quickest path from concept to delivery.", result.Bounds.Bottom + 25);
-
-			PdfPen redPen = new PdfPen(PdfBrushes.Red, 2);
-			g.DrawLine(redPen, new PointF(40, result.Bounds.Bottom + 92), new PointF(40, result.Bounds.Bottom + 145));
-			float headerBulletsXposition = 40;
-			var txtElement = new PdfTextElement("The Experts");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 20);
-			txtElement.Draw(page, new RectangleF(headerBulletsXposition + 5, result.Bounds.Bottom + 90, 450, 200));
-
-			PdfPen violetPen = new PdfPen(PdfBrushes.Violet, 2);
-			g.DrawLine(violetPen, new PointF(headerBulletsXposition + 280, result.Bounds.Bottom + 92), new PointF(headerBulletsXposition + 280, result.Bounds.Bottom + 145));
-			txtElement = new PdfTextElement("Accurate Estimates");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 20);
-			result = txtElement.Draw(page, new RectangleF(headerBulletsXposition + 290, result.Bounds.Bottom + 90, 450, 200));
-
-			txtElement = new PdfTextElement("A substantial number of .NET reporting applications use our frameworks");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 11, PdfFontStyle.Regular);
-			result = txtElement.Draw(page, new RectangleF(headerBulletsXposition + 5, result.Bounds.Bottom + 5, 250, 200));
+			g.DrawString ("Propriedade", subHeadingFont, new PdfSolidBrush (black), new PointF (25, 150));
+			g.DrawString ("Valor atual", subHeadingFont, new PdfSolidBrush (black), new PointF (175, 150));
+			g.DrawString ("Nível adequado", subHeadingFont, new PdfSolidBrush (black), new PointF (325, 150));
+			g.DrawString ("Classe", subHeadingFont, new PdfSolidBrush (black), new PointF (475, 150));
 
 
-			txtElement = new PdfTextElement("Given our expertise, you can expect estimates to be accurate.");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 11, PdfFontStyle.Regular);
-			result = txtElement.Draw(page, new RectangleF(headerBulletsXposition + 290, result.Bounds.Y, 250, 200));
+			//pH (CaCl2)
+			var valorAtual = (BindingContext as InterpretacaoViewModel).Analise.Ph.ToString ();
+			var valorAdequado = "4,9 a 5,5";
+			var classe = (BindingContext as InterpretacaoViewModel).InterpretacaoPh;
+
+			var y = BodyContent (g, "pH (CaCl2)", valorAtual, valorAdequado, classe, 180, grayLight);
+
+			//P
+			valorAtual = (BindingContext as InterpretacaoViewModel).Analise.P.ToString ();
+			valorAdequado = TexturaPConverter ((BindingContext as InterpretacaoViewModel).InterpretacaoTextura);
+			classe = (BindingContext as InterpretacaoViewModel).InterpretacaoP;
+
+			y = BodyContent (g, "P", valorAtual, valorAdequado, classe, y, white);
+
+			//Ca
+			valorAtual = (BindingContext as InterpretacaoViewModel).Analise.Ca.ToString ();
+			valorAdequado = "1,5 a 7,0";
+			classe = (BindingContext as InterpretacaoViewModel).InterpretacaoCa;
+
+			y = BodyContent (g, "Ca", valorAtual, valorAdequado, classe, y, grayLight);
+
+			//Mg
+			valorAtual = (BindingContext as InterpretacaoViewModel).Analise.Mg.ToString ();
+			valorAdequado = "0,5 a 2,0";
+			classe = (BindingContext as InterpretacaoViewModel).InterpretacaoMg;
+
+			y = BodyContent (g, "Mg", valorAtual, valorAdequado, classe, y, white);
+
+			//V(%)
+			valorAtual = (BindingContext as InterpretacaoViewModel).Analise.V.ToString ();
+			valorAdequado = "36 a 60";
+			classe = (BindingContext as InterpretacaoViewModel).InterpretacaoV;
+
+			y = BodyContent (g, "V(%)", valorAtual, valorAdequado, classe, y, grayLight);
+
+			//CTC(T)
+			valorAtual = (BindingContext as InterpretacaoViewModel).Analise.CTC.ToString ();
+			valorAdequado = TexturaCTCConverter ((BindingContext as InterpretacaoViewModel).InterpretacaoTextura);
+			classe = (BindingContext as InterpretacaoViewModel).InterpretacaoCtc;
+
+			y = BodyContent (g, "CTC(T)", valorAtual, valorAdequado, classe, y, white);
+
+			//Matéria Orgânica
+			valorAtual = (BindingContext as InterpretacaoViewModel).Analise.MateriaOrganica.ToString ();
+			valorAdequado = TexturaMoConverter ((BindingContext as InterpretacaoViewModel).InterpretacaoTextura);
+			classe = (BindingContext as InterpretacaoViewModel).InterpretacaoMo;
+
+			y = BodyContent (g, "Matéria Orgânica", valorAtual, valorAdequado, classe, y, grayLight);
+
+			//Ca/K
+			valorAtual = (BindingContext as InterpretacaoViewModel).Analise.CaK.ToString ();
+			valorAdequado = "15 a 25";
+			classe = (BindingContext as InterpretacaoViewModel).InterpretacaoCaK;
+
+			y = BodyContent (g, "Ca/K", valorAtual, valorAdequado, classe, y, white);
+
+			//Mg/K
+			valorAtual = (BindingContext as InterpretacaoViewModel).Analise.MgK.ToString ();
+			valorAdequado = "5 a 15";
+			classe = (BindingContext as InterpretacaoViewModel).InterpretacaoMgK;
+
+			y = BodyContent (g, "Mg/K", valorAtual, valorAdequado, classe, y, grayLight);
+
+			//m%
+			valorAtual = (BindingContext as InterpretacaoViewModel).Analise.M.ToString ();
+			valorAdequado = "-";
+			classe = (BindingContext as InterpretacaoViewModel).InterpretacaoM;
+
+			y = BodyContent (g, "m%", valorAtual, valorAdequado, classe, y, white);
+
+			PdfFont footerBoldFont = new PdfStandardFont (PdfFontFamily.Helvetica, 10, PdfFontStyle.Bold);
+			PdfFont footerFont = new PdfStandardFont (PdfFontFamily.Helvetica, 10);
 
 
-			PdfPen greenPen = new PdfPen(PdfBrushes.Green, 2);
-			g.DrawLine(greenPen, new PointF(40, result.Bounds.Bottom + 32), new PointF(40, result.Bounds.Bottom + 85));
-
-			txtElement = new PdfTextElement("Product Licensing");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 20);
-			txtElement.Draw(page, new RectangleF(headerBulletsXposition + 5, result.Bounds.Bottom + 30, 450, 200));
-
-			PdfPen bluePen = new PdfPen(PdfBrushes.Blue, 2);
-			g.DrawLine(bluePen, new PointF(headerBulletsXposition + 280, result.Bounds.Bottom + 32), new PointF(headerBulletsXposition + 280, result.Bounds.Bottom + 85));
-			txtElement = new PdfTextElement("About Syncfusion");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 20);
-			result = txtElement.Draw(page, new RectangleF(headerBulletsXposition + 290, result.Bounds.Bottom + 30, 450, 200));
-
-			txtElement = new PdfTextElement("Solution packages can be combined with product licensing for great cost savings.");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 11, PdfFontStyle.Regular);
-			result = txtElement.Draw(page, new RectangleF(headerBulletsXposition + 5, result.Bounds.Bottom + 5, 250, 200));
-
-
-			txtElement = new PdfTextElement("Syncfusion has more than 7,000 customers including large financial institutions and Fortune 100 companies.");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 11, PdfFontStyle.Regular);
-			result = txtElement.Draw(page, new RectangleF(headerBulletsXposition + 290, result.Bounds.Y, 250, 200));
-
-			var assembly = typeof(App).GetTypeInfo().Assembly;
-			Stream imgStream = assembly.GetManifestResourceStream("Solum.teste.jpg");
-			
-			g.DrawImage(PdfImage.FromStream(imgStream), 280, 600, 300, 170);
-
-			g.DrawString("All trademarks mentioned belong to their owners.", new PdfStandardFont(PdfFontFamily.TimesRoman, 8, PdfFontStyle.Italic), PdfBrushes.White, new PointF(10, g.ClientSize.Height - 30));
-			PdfTextWebLink linkAnnot = new PdfTextWebLink();
+			//Footer
+			g.DrawRectangle (new PdfSolidBrush (green), new RectangleF (0, page.Graphics.ClientSize.Height - 80, page.Graphics.ClientSize.Width, 5));
+			g.DrawString ("Base de cálculo: Região Centro-Oeste", footerFont, new PdfSolidBrush (black), new PointF (20, page.Graphics.ClientSize.Height - 60));
+			g.DrawString ("Relatório gerado pelo aplicativo Solum", footerBoldFont, new PdfSolidBrush (black), new PointF (20, page.Graphics.ClientSize.Height - 40));
+			g.DrawString ("Desenvolvido por Sydy Tecnologia", footerFont, new PdfSolidBrush (black), new PointF (page.Graphics.ClientSize.Width - 175, page.Graphics.ClientSize.Height - 60));
+			PdfTextWebLink linkAnnot = new PdfTextWebLink ();
 			linkAnnot.Url = "http://www.sydy.com.br";
 			linkAnnot.Text = "www.sydy.com.br";
-			linkAnnot.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 8, PdfFontStyle.Italic);
-			linkAnnot.Brush = PdfBrushes.White;
-			linkAnnot.DrawTextWebLink(page, new PointF(g.ClientSize.Width - 100, g.ClientSize.Height - 30));
+			linkAnnot.Font = footerBoldFont;
+			linkAnnot.Brush = new PdfSolidBrush (black);
+			linkAnnot.DrawTextWebLink (page, new PointF (page.Graphics.ClientSize.Width - 105, page.Graphics.ClientSize.Height - 40));
+
 			MemoryStream stream = new MemoryStream();
 			doc.Save(stream);
 			doc.Close(true);
@@ -165,46 +200,64 @@ namespace Solum.Pages
 			DependencyService.Get<IPdfViewer>().PreviewPdf(stream);
 		}
 
-		private PdfLayoutResult BodyContent(string text, float yPosition)
+		private float BodyContent (PdfGraphics pg, string propriedade, string valorAtual, string valorAdequado, string classe, float yPosition, Syncfusion.Drawing.Color color)
 		{
-			float headerBulletsXposition = 35;
-			PdfTextElement txtElement = new PdfTextElement("3");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.ZapfDingbats, 16);
-			txtElement.Brush = new PdfSolidBrush(violet);
-			txtElement.StringFormat = new PdfStringFormat();
-			txtElement.StringFormat.WordWrap = PdfWordWrapType.Word;
-			txtElement.StringFormat.LineLimit = true;
-			txtElement.Draw(page, new RectangleF(headerBulletsXposition, yPosition, 320, 100));
+			PdfFont textFont = new PdfStandardFont (PdfFontFamily.Helvetica, 12);
 
-			txtElement = new PdfTextElement(text);
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 17);
-			txtElement.Brush = new PdfSolidBrush(white);
-			txtElement.StringFormat = new PdfStringFormat();
-			txtElement.StringFormat.WordWrap = PdfWordWrapType.Word;
-			txtElement.StringFormat.LineLimit = true;
-			PdfLayoutResult result = txtElement.Draw(page, new RectangleF(headerBulletsXposition + 25, yPosition, 450, 130));
-			return result;
+			pg.DrawRectangle (new PdfSolidBrush (gray), new RectangleF (20, yPosition, page.Graphics.ClientSize.Width - 40, 30));
+			pg.DrawRectangle (new PdfSolidBrush (color), new RectangleF (21, yPosition + 1, page.Graphics.ClientSize.Width - 42, 28));
+
+			pg.DrawString (propriedade, textFont, new PdfSolidBrush (black), new PointF (25, yPosition + 8));
+			pg.DrawString (valorAtual, textFont, new PdfSolidBrush (black), new PointF (175, yPosition + 8));
+			pg.DrawString (valorAdequado, textFont, new PdfSolidBrush (black), new PointF (325, yPosition + 8));
+			pg.DrawString (classe, textFont, new PdfSolidBrush (black), new PointF (475, yPosition + 8));
+
+			return yPosition + 29;
 		}
 
-		private PdfLayoutResult HeaderPoints(string text, float yPosition)
-		{
-			float headerBulletsXposition = 220;
-			PdfTextElement txtElement = new PdfTextElement("l");
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.ZapfDingbats, 10);
-			txtElement.Brush = new PdfSolidBrush(violet);
-			txtElement.StringFormat = new PdfStringFormat();
-			txtElement.StringFormat.WordWrap = PdfWordWrapType.Word;
-			txtElement.StringFormat.LineLimit = true;
-			txtElement.Draw(page, new RectangleF(headerBulletsXposition, yPosition, 300, 100));
+		string TexturaPConverter(string textura){
+			switch (textura) {
+			case "Arenosa":
+				return "18,1 a 25";
+			case "Média":
+				return "15,1 a 20";
+			case "Argilosa":
+				return "8,1 a 12";
+			case "Muito argilosa":
+				return "4,1 a 6";
+			default:
+				return "";
+			}
+		}
 
-			txtElement = new PdfTextElement(text);
-			txtElement.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 11);
-			txtElement.Brush = new PdfSolidBrush(white);
-			txtElement.StringFormat = new PdfStringFormat();
-			txtElement.StringFormat.WordWrap = PdfWordWrapType.Word;
-			txtElement.StringFormat.LineLimit = true;
-			PdfLayoutResult result = txtElement.Draw(page, new RectangleF(headerBulletsXposition + 20, yPosition, 320, 100));
-			return result;
+		string TexturaCTCConverter(string textura){
+			switch (textura) {
+			case "Arenosa":
+				return "4,1 a 6,0";
+			case "Média":
+				return "6,1 a 9,0";
+			case "Argilosa":
+				return "9,1 a 13,5";
+			case "Muito argilosa":
+				return "12,1 a 18,0";
+			default:
+				return "";
+			}
+		}
+
+		string TexturaMoConverter(string textura){
+			switch (textura) {
+			case "Arenosa":
+				return "11 a 15";
+			case "Média":
+				return "21 a 30";
+			case "Argilosa":
+				return "31 a 45";
+			case "Muito argilosa":
+				return "36 a 52";
+			default:
+				return "";
+			}
 		}
 	}
 }
