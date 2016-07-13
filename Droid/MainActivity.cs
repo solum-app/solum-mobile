7119ce4bd935cd14826f8a7c38260f9275115c6c
sidneipolo <sidneipolo.sp@gmail.com>
2016-07-13 17:27:43 -0400

    Hockeyapp adicionado ao android

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -2,14 +2,26 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using HockeyApp;
+using HockeyApp.Metrics;
 
 namespace Solum.Droid
 {
 	[Activity (Label = "Solum.Droid", Icon = "@drawable/icon", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
+
+		const string HOCKEYAPP_KEY = "c4aa5557f72547d3b39966736013ff92";
+
 		protected override void OnCreate (Bundle bundle)
 		{
+
+			//HockeyApp setup
+			#if (!DEBUG)
+			CrashManager.Register (this, HOCKEYAPP_KEY, new CrashManagerSettings ());
+			MetricsManager.Register(this, Application, HOCKEYAPP_KEY);
+			#endif
+
 			TabLayoutResource = Resource.Layout.Tabbar;
 			ToolbarResource = Resource.Layout.Toolbar;
 			base.SetTheme(Resource.Style.MyTheme);
@@ -18,6 +30,14 @@ namespace Solum.Droid
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 
 			LoadApplication (new App ());
+		}
+	}
+
+	public class CrashManagerSettings : CrashManagerListener
+	{
+		public override bool ShouldAutoUploadCrashes ()
+		{
+			return true;
 		}
 	}
 }
--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -11,17 +11,20 @@ namespace Solum.iOS
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
+		const string HOCKEYAPP_KEY = "48f0548fc8a044709b96170f1979a0c6";
+
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
+			//HockeyApp setup
+			#if (!DEBUG)
+			var manager = BITHockeyManager.SharedHockeyManager;
+			manager.Configure (HOCKEYAPP_KEY);
+			manager.CrashManager.CrashManagerStatus = BITCrashManagerStatus.AutoSend;
+			manager.StartManager ();
+			#endif
+
 			global::Xamarin.Forms.Forms.Init ();
 			UIApplication.SharedApplication.SetStatusBarStyle(UIStatusBarStyle.LightContent, false);
-
-			//Get the shared instance
-			var manager = BITHockeyManager.SharedHockeyManager;
-			//Configure it to use our APP_ID
-			manager.Configure("48f0548fc8a044709b96170f1979a0c6");
-			//Start the manager
-			manager.StartManager();
 
 			LoadApplication (new App ());
 
