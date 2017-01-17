e89969029f5fbaa87a7e5c2431d8da3f7365abad
luylucas <luylucas10@gmail.com>
2017-01-17 09:52:59 -0300

    Detalhes de Fazenda com xaml para tela de cadastro de talhão

---
+++ Solum/ViewModel/FazendaDetalhesViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Realms;
+using Solum.Models;
+using Xamarin.Forms;
+
+namespace Solum.ViewModel
+{
+    public class FazendaDetalhesViewModel : BaseViewModel
+    {
+        private Fazenda _fazenda;
+        private IList _talhoesList;
+        private bool _hasItems;
+        private Realm _realm;
+        public FazendaDetalhesViewModel(INavigation navigation, Fazenda item) : base(navigation)
+        {
+            //            _realm = Realm.GetInstance();
+            _fazenda = item;
+            HasItems = false;
+        }
+
+        public bool HasItems
+        {
+            get { return _hasItems; }
+            set { SetPropertyChanged(ref _hasItems, value); }
+        }
+
+        public Fazenda Fazenda
+        {
+            get { return _fazenda; }
+            set { SetPropertyChanged(ref _fazenda, value); }
+        }
+
+        public IList TalhoesList
+        {
+            get { return _talhoesList; }
+            set { SetPropertyChanged(ref _talhoesList, value); }
+        }
+    }
+}
