62c42e6515a6df5641ce8bd4cccf481217382359
luylucas <luylucas10@gmail.com>
2017-01-17 09:48:18 -0300

    Alteração no model de Cidade

--- Solum/Models/Cidade.cs
+++ Solum/Models/Cidade.cs
@@ -7,6 +7,7 @@ namespace Solum.Models
         [PrimaryKey]
         public string Id { get; set; }
         public string Nome { get; set; }
+        public string EstadoId { get; set; }
         public Estado Estado { get; set; }
 
         public override string ToString()
