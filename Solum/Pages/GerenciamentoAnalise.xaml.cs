c488e21ac63902e6434eb779719ff91ede15c700
luylucas <luylucas10@gmail.com>
2017-01-23 10:54:49 -0300

    Tela de gerenciamento de análise

---
+++ Solum/Pages/GerenciamentoAnalise.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Solum.Models;
+using Solum.ViewModel;
+using Xamarin.Forms;
+
+namespace Solum.Pages
+{
+    public partial class GerenciamentoAnalise : ContentPage
+    {
+        public GerenciamentoAnalise(Analise analise)
+        {
+            InitializeComponent();
+            BindingContext = new GerenciamentoAnaliseViewModel(Navigation, analise);
+            NavigationPage.SetBackButtonTitle(this, Settings.BackButtonTitle);
+        }
+    }
+}
---
+++ Solum/ViewModel/GerenciamentoAnaliseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+using Solum.Models;
+using Xamarin.Forms;
+
+namespace Solum.ViewModel
+{
+    public class GerenciamentoAnaliseViewModel : BaseViewModel
+    {
+        public GerenciamentoAnaliseViewModel(INavigation navigation, Analise analise) : base(navigation)
+        {
+            _analise = analise;
+        }
+
+        private ICommand _abrirTelaInterpretacaoCommand;
+        private ICommand _abrirTelaCalagemCommand;
+        private ICommand _abrirTelaCorretivaCommand;
+        private ICommand _abrirTelaSemeaduraCommand;
+        private ICommand _abrirTelaCoberturaCommand;
+
+        private Analise _analise;
+
+    }
+}
