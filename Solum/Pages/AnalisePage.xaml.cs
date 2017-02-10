65cfc2e49d9f5cbfe27c60e84c73c7462569924f
luylucas <luylucas10@gmail.com>
2017-02-10 11:31:09 -0300

    SOL-82 - AnálisePage

--- Solum/Pages/AnalisePage.xaml.cs
+++ Solum/Pages/AnalisePage.xaml.cs
@@ -1,54 +1,63 @@
-﻿using Xamarin.Forms;
-using Solum.ViewModel;
+﻿using System;
 using Solum.Models;
-using System;
+using Solum.ViewModel;
+using Xamarin.Forms;
 
 namespace Solum.Pages
 {
-	public partial class AnalisePage : ContentPage
-	{
-		public AnalisePage ()
-		{
+    public partial class AnalisePage : ContentPage
+    {
+        public AnalisePage()
+        {
             InitializeComponent();
-            BindingContext = new AnaliseViewModel (Navigation);
-            NavigationPage.SetBackButtonTitle(this, Settings.BackButtonTitle);
-		}
-
-		public AnalisePage (Analise analise)
-		{
-            InitializeComponent();
-            BindingContext = new AnaliseViewModel (Navigation, analise);
+            BindingContext = new AnaliseViewModel(Navigation);
             NavigationPage.SetBackButtonTitle(this, Settings.BackButtonTitle);
         }
 
-        //void Init()
-        //{
+        public AnalisePage(Analise analise)
+        {
+            InitializeComponent();
+            BindingContext = new AnaliseViewModel(Navigation, analise);
+            NavigationPage.SetBackButtonTitle(this, Settings.BackButtonTitle);
+        }
 
-        //    NavigationPage.SetBackButtonTitle(this, "Voltar");
-        //    fazendaEntry.Completed += (s, e) => talhaoEntry.Focus();
-        //    talhaoEntry.Completed += (s, e) => datePicker.Focus();
-        //    phEntry.Completed += (s, e) => pEntry.Focus();
-        //    pEntry.Completed += (s, e) => kEntry.Focus();
-        //    kEntry.Completed += (s, e) => caEntry.Focus();
-        //    caEntry.Completed += (s, e) => mgEntry.Focus();
-        //    mgEntry.Completed += (s, e) => alEntry.Focus();
-        //    alEntry.Completed += (s, e) => hEntry.Focus();
-        //    hEntry.Completed += (s, e) => moEntry.Focus();
-        //    moEntry.Completed += (s, e) => areiaEntry.Focus();
-        //    areiaEntry.Completed += (s, e) => sliteEntry.Focus();
-        //    sliteEntry.Completed += (s, e) => argilaEntry.Focus();
-        //}
+        private void OnEntryFocused(object sender, EventArgs e)
+        {
+            if (Device.OS == TargetPlatform.Android)
+                boxSpacing.IsVisible = !boxSpacing.IsVisible;
+        }
 
-        void OnEntryFocused (object sender, EventArgs e){
-			if (Device.OS == TargetPlatform.Android)
-				boxSpacing.IsVisible = !boxSpacing.IsVisible;
-		}
+        private void OnEntryUnfocused(object sender, EventArgs e)
+        {
+            if (Device.OS == TargetPlatform.Android)
+                boxSpacing.IsVisible = false;
+        }
 
-		void OnEntryUnfocused(object sender, EventArgs e)
-		{
-			if (Device.OS == TargetPlatform.Android)
-				boxSpacing.IsVisible = false;
-		}
-	}
+        private void AnaliseNameEntry_OnCompleted(object sender, EventArgs e) => FazendaLabel.Focus();
+
+        private void PhEntry_OnCompleted(object sender, EventArgs e) => FosforoEntry.Focus();
+
+        private void FosforoEntry_OnCompleted(object sender, EventArgs e) => PotassioEntry.Focus();
+
+        private void PotassioEntry_OnCompleted(object sender, EventArgs e) => CalcioEntry.Focus();
+
+        private void CalcioEntry_OnCompleted(object sender, EventArgs e) => MagnesioEntry.Focus();
+
+        private void MagnesioEntry_OnCompleted(object sender, EventArgs e) => AluminioEntry.Focus();
+
+        private void AluminioEntry_OnCompleted(object sender, EventArgs e) => HidrogenioEntry.Focus();
+
+        private void HidrogenioEntry_OnCompleted(object sender, EventArgs e) => MateriaOrganicaEntry.Focus();
+
+        private void MateriaOrganicaEntry_OnCompleted(object sender, EventArgs e) => AreiaEntry.Focus();
+
+        private void AreiaEntry_OnCompleted(object sender, EventArgs e) => SilteEntry.Focus();
+
+        private void SilteEntry_OnCompleted(object sender, EventArgs e) => ArgilaEntry.Focus();
+
+        private void ArgilaEntry_OnCompleted(object sender, EventArgs e)
+        {
+            ArgilaEntry.Unfocus();
+        }
+    }
 }
-
