70fd42a01f5efc4e1279b854142473c45e7e3848
luylucas <luylucas10@gmail.com>
2017-02-13 14:12:36 -0300

    SOL-82 - CalagemPage

--- Solum/Pages/CalagemPage.xaml.cs
+++ Solum/Pages/CalagemPage.xaml.cs
@@ -1,15 +1,20 @@
-﻿using Solum.ViewModel;
+﻿using System;
+using Solum.ViewModel;
 using Xamarin.Forms;
 
 namespace Solum.Pages
 {
-	public partial class CalagemPage : ContentPage
-	{
-		public CalagemPage(string analiseId)
-		{
-			InitializeComponent();
+    public partial class CalagemPage : ContentPage
+    {
+        public CalagemPage(string analiseId)
+        {
+            InitializeComponent();
             BindingContext = new CalagemViewModel(Navigation, analiseId);
             NavigationPage.SetBackButtonTitle(this, Settings.BackButtonTitle);
-		}
-	}
+        }
+
+        private void V2Picker_OnSelectedIndexChanged(object sender, EventArgs e) => PrntEntry.Focus();
+
+        private void PrntEntry_OnCompleted(object sender, EventArgs e) => ProfundidadePicker.Focus();
+    }
 }
