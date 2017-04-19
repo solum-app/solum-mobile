31ff2d814d4d6524d04d7884051929df8c301ad4
sidneipolo <sidnei@sydy.com.br>
2017-04-19 11:37:21 -0400

    FileAccessHelper iOS modified

--- iOS/Helpers/FileAccessHelper.cs
+++ iOS/Helpers/FileAccessHelper.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.IO;
+using Foundation;
+
 namespace Solum.iOS
 {
 	public class FileAccessHelper
 	{
-		public FileAccessHelper()
+		public static string GetLocalFilePath(string filename)
 		{
+			string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+			if (!Directory.Exists(libFolder))
+			{
+				Directory.CreateDirectory(libFolder);
+			}
+
+			string dbPath = Path.Combine(libFolder, filename);
+
+			CopyDatabaseIfNotExists(dbPath);
+
+			return dbPath;
+		}
+
+		private static void CopyDatabaseIfNotExists(string dbPath)
+		{
+			if (!File.Exists(dbPath))
+			{
+				var existingDb = NSBundle.MainBundle.PathForResource("Solum", "db");
+				File.Copy(existingDb, dbPath);
+			}
 		}
 	}
 }
