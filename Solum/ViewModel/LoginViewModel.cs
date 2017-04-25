26fde53c2ca3207ad17c43cd37c579d3b19aa861
sidneipolo <sidnei@sydy.com.br>
2017-04-25 17:03:49 -0400

    Clean nos pacotes do projeto (reverted from commit 18f47743c1af6fcecc9ea6639c705978d42bdc2f)

--- Solum/ViewModel/LoginViewModel.cs
+++ Solum/ViewModel/LoginViewModel.cs
@@ -77,6 +77,8 @@ namespace Solum.ViewModel
                 return;
             IsBusy = true;
 
+			var a = Acr.UserDialogs.UserDialogs.Instance.Loading("Teste");
+
             if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
             {
                 MessagesResource.LoginCredenciaisNulas.ToDisplayAlert(MessageType.Aviso);
